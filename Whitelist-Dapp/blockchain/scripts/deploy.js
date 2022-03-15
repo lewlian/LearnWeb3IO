841070e6829ac05c885f4da16f51d9269241670a
lewlian <seanlewtengsiong@gmail.com>
2022-03-15 17:51:46 +0800

    Initial Commit

---
+++ Whitelist-Dapp/blockchain/scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+	const whitelistContract = await ethers.getContractFactory('Whitelist');
+	const deployedWhitelistContract = await whitelistContract.deploy(10);
+
+	await deployedWhitelistContract.deployed();
+
+	console.log('Whitelist Contract deployed to:', deployedWhitelistContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
