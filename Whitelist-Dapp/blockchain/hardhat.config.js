4fb766145b92ea9aa0af1d5af87c64ffb181fb5f
lewlian <seanlewtengsiong@gmail.com>
2022-03-27 00:40:53 +0800

    pushing sc and fe for nft-collection

--- Whitelist-Dapp/blockchain/hardhat.config.js
+++ Whitelist-Dapp/blockchain/hardhat.config.js
@@ -1,8 +1,10 @@
 require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
 require('dotenv').config({ path: '.env' });
 
 const INFURA_API_KEY_URL = process.env.INFURA_API_KEY_URL;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
 	solidity: '0.8.4',
@@ -11,5 +13,10 @@ module.exports = {
 			url: INFURA_API_KEY_URL,
 			accounts: [RINKEBY_PRIVATE_KEY],
 		},
+	},
+	etherscan: {
+		// Your API key for Etherscan
+		// Obtain one at https://etherscan.io/
+		apiKey: ETHERSCAN_API_KEY,
 	},
 };
--- Whitelist-Dapp/blockchain/scripts/deploy.js
+++ Whitelist-Dapp/blockchain/scripts/deploy.js
@@ -1,4 +1,5 @@
 const { ethers } = require('hardhat');
+const { METADATA_URL } = require('../constants');
 
 async function main() {
 	const whitelistContract = await ethers.getContractFactory('Whitelist');
@@ -7,6 +8,16 @@ async function main() {
 	await deployedWhitelistContract.deployed();
 
 	console.log('Whitelist Contract deployed to:', deployedWhitelistContract.address);
+	// Address of the whitelist contract that you deployed in the previous module
+	const whitelistCA = deployedWhitelistContract.address;
+	// URL from where we can extract the metadata for a Crypto Dev NFT
+
+	console.log('Deploying the CryptoDevs contract...');
+
+	const cryptoDevContract = await ethers.getContractFactory('CryptoDevs');
+	const deployedCryptoDevContract = await cryptoDevContract.deploy(METADATA_URL, whitelistCA);
+
+	console.log('NFT Contract deployed to:', deployedCryptoDevContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
