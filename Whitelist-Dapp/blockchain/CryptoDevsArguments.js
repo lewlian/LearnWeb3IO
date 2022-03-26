1f3c2a741f0f5a1a38ddb35b5d061e158df2987c
lewlian <seanlewtengsiong@gmail.com>
2022-03-27 01:14:20 +0800

    updated api endpoint

--- Whitelist-Dapp/blockchain/CryptoDevsArguments.js
+++ Whitelist-Dapp/blockchain/CryptoDevsArguments.js
@@ -1,1 +1,1 @@
-module.exports = ['https://nft-collection-sneh1999.vercel.app/api/', '0x874028aFd6Faa904D50f72FDc18068aE6C22d50e'];
+module.exports = ['https://learn-web3-io.vercel.app/', '0x177EF4D0e1dEa29C135BD4B6BF0F215bfc389712'];
--- Whitelist-Dapp/blockchain/constants/index.js
+++ Whitelist-Dapp/blockchain/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = 'address-of-the-whitelist-contract';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://learn-web3-io.vercel.app/';
+const METADATA_URL = 'https://learn-web3-io.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
