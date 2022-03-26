389dbbeaeadfe913b2b48dac7635c6a1ddccf277
lewlian <seanlewtengsiong@gmail.com>
2022-03-27 00:51:08 +0800

    added endpoint for token

--- Whitelist-Dapp/blockchain/constants/index.js
+++ Whitelist-Dapp/blockchain/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = 'address-of-the-whitelist-contract';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://learn-web3-io.vercel.app/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
